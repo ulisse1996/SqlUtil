51ce14189c4ecf5dd1be3883a2585e2872f83a83
Donato Leone <d.leone@quidinfo.it>
2020-04-29 17:44:13 +0200

    Remove code smell , add badges

--- src/main/java/it/donatoleone/sqlutil/impl/FromBuilder.java
+++ src/main/java/it/donatoleone/sqlutil/impl/FromBuilder.java
@@ -3,7 +3,6 @@ package it.donatoleone.sqlutil.impl;
 import it.donatoleone.sqlutil.interfaces.*;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
